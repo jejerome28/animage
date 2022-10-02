e61056b947fdb205605b8186fe3ee22964871230
jejerome <goyalajerome@gmail.com>
2022-10-02 18:08:56 +0800

    refactored authentication

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
 const path = require('path');
 const mongoose = require('mongoose');
 const user = require('./model/userModel');
-const bcrypt = require('bcrypt');
 const passport = require('passport');
-const local_strategy = require('passport-local');
 const session = require('express-session');
 const dbString = 'mongodb://localhost:27017/aniMage';
 const mongoStore = require('connect-mongo');
 const storeSession = mongoStore.create({mongoUrl:dbString, collectionName: 'sessions'});
 const routes = require('./routes/routes');
 const method_override = require('method-override');
+const auth = require('./controller/auth');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -33,6 +31,11 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+
+auth(
+    passport,
+    username => user.findOne({username})
+)
 app.use((req, res, next)=>{
     console.log(req.session);
     console.log(req.user);
@@ -42,39 +45,6 @@ app.use((req, res, next)=>{
 //create custom connection
 mongoose.createConnection(dbString, (err,result)=>{if(err)throw err; console.log('connected')})
 
-
-async function verify(username, password, done) {
-
-    try{
-    const userAccount = await user.findOne({ username });
-    const isValid = await bcrypt.compare(password, userAccount.password)
-    
-    console.log(isValid);
-    
-    if(!userAccount){return done(null,false)}
-    if(isValid){return done(null, userAccount)}
-    else{return done(null, false)}
-    }
-    catch(err){
-        console.log(err)
-    }
-
-    
-}
-const strat = new local_strategy(verify);
-
-passport.use(strat);
-passport.serializeUser((user,done)=>{
-    done(null, user.id);
-})
-
-passport.deserializeUser((userId, done)=>{
-    user.findById(userId)
-        .then((user)=>{
-            done(null,user);
-        })
-        .catch(err => done(err));
-})
 
 //use all routes
 app.use('/', routes);
