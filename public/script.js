41aa2afaa301923c47fcd17c502c890cf4c39119
jejerome <goyalajerome@gmail.com>
2022-10-07 20:38:50 +0800

    added comment area

---
+++ public/script.js
@@ -0,0 +1,7 @@
+// const counter  = () => {
+//     let count = document.getElementById('addComment');
+//     document.getElementById('counter').innerText = 'hello';
+// }
+
+// document.getElementById("counter").innerHTML = "hello";
+// document.getElementsByClassName('add_comment').getElementsById('counter').innerHTML = 'hello';
