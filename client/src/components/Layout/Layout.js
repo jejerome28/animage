b783d6a83e265edcfb5f5dba1adbf31f917500cb
jejerome <goyalajerome@gmail.com>
2022-11-07 16:26:43 +0800

    added layout

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import style from './Layout.module.css'
+import Home from "../Home/Home";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+const Layout = ()=> {
+    return(
+        <>
+        <div className={style.wrapper}>
+            <div className={style.nav}>
+                <Navbar/>
+            </div>
+            <div className={style.home}>
+                <Home/>
+            </div>
+            <div className={style.footer}>
+                <Footer/>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Layout
