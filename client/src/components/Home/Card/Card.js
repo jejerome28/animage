cbc9727b738226c5b2ca8397192eb309f0e7073d
jejerome <goyalajerome@gmail.com>
2022-11-12 14:12:02 +0800

    created slider using classes in react

--- client/src/components/Home/Card/Card.js
+++ client/src/components/Home/Card/Card.js
@@ -2,51 +2,56 @@ import React from "react";
 import style from './Card.module.css'
 import axios from 'axios'
 
-// const Card = (props)=>{
-//     return(
-//         <>
-//         <div className={style.card}>
-//             <img src={props.results.images.jpg.large_image_url} alt='anime pic' ></img>
-//         </div>
-//         </>
-//     )
-// }
-class Card extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {results: ' '}
-    }
-    
-    async componentDidMount(){
-        const getAani = await this.getAnime('https://api.jikan.moe/v4/top/anime', {filter: 'bypopularity', page:1});
-        this.setState({results: getAani});
-        console.log(this.state.results)
-    }
-    
-    async componentWillUnmount(){
-        clearInterval(await this.getAani)
-    }
+const Card = ({anime})=>{
+    return(
+        <>
+        <div className={style.card}>
+            <img src={anime.images.webp.small_image_url} alt='anime pic' ></img>
+            <p>{anime.title}</p>
+        </div>
+        </>
+    )
+}
+// mauuna muna na marender bago mag mount
 
-    getAnime = async (url, params={}) => {
-        const results = await axios({
-            url: url,
-            method: 'GET',
-            params: params
-        })
+// class Card extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {results: {}}
+//     }
+    
+//     async componentDidMount(){
+//         const getAani = await this.getAnime('https://api.jikan.moe/v4/top/anime', {filter: 'bypopularity', page:1});
+//         this.setState({results: getAani});
+//         console.log(this.state.results[0].images);
+//         // console.log;
+//     }
+    
+    
+//     async componentWillUnmount(){
+//         clearInterval(await this.getAani);
+//     }
+
+//     getAnime = async (url, params={}) => {
+//         const results = await axios({
+//             url: url,
+//             method: 'GET',
+//             params: params
+//         })
         
     
-    return results.data.data
-    }
+//     return results.data.data
+//     }
     
-    render(){
-        return(
-            <div>
-                    
-            <p>{this.state.results[0].title}</p>
-            </div>
-        )
-    }
+//     render(){
+//         return(
+//             <div className={style.card}>
+//                 {/* <img src={this.state.results[0].image_url} alt={this.state.results[0].title} /> */}
+//                 {/* <p>{this.state.results[0].title}</p> */}
+//             </div>
+//         )
+//     }
 
-}
+// }
 
 export default Card
--- client/src/components/Home/Slider/Slider.js
+++ client/src/components/Home/Slider/Slider.js
@@ -1,54 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './Slider.module.css'
 import Card from "../Card/Card";
+import axios from "axios";
 
 
 
-// class Ani extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {results: ''}
-//     }
+class Slider extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {results: []}
+        
+    }
     
-//     async componentDidMount(){
-//         const getAani = await this.getAnime('https://api.jikan.moe/v4/top/anime', {filter: 'bypopularity', page:1});
-//         this.setState({results: getAani});
-//         console.log(this.state.results[0])
-//     }
+    async componentDidMount(){
+        const getAani = await this.getAnime('https://api.jikan.moe/v4/top/anime', {filter: 'bypopularity', page:1});
+        this.setState({results: getAani},()=>{console.log(this.state.results[0])});
+    }
     
-//     async componentWillUnmount(){
-//         clearInterval(await this.getAani)
-//     }
 
-//     getAnime = async (url, params={}) => {
-//         const results = await axios({
-//             url: url,
-//             method: 'GET',
-//             params: params
-//         })
+    async componentWillUnmount(){
+        clearInterval(await this.getAani)
+    }
+     getAnime = async (url, params={}) => {
+        const results = await axios({
+            url: url,
+            method: 'GET',
+            params: params
+        })
         
     
-//     return results.data.data
-//     }
+        return results.data.data
+    }
     
-//     render(){
-//         return(
-//             <>
-//             <p>{this.state.results}</p>
-//             </>
-//         )
-//     }
+    render(){
+        const {results} = this.state
+        const sliced = results.slice(0,7)
+        return(
+        <div className={style.wrapper}>
+            {sliced.map(ani=>(<Card anime={ani}  key={ani.mal_id}/>))}
+            
+        </div>
+        )
+    }
 
+}
+
+// const getAnime = async (url, params={}) => {
+    
+//     const results = await axios({
+//         url: url,
+//         method: 'GET',
+//         params: params
+//     })
+    
+
+//     return results.data.data
 // }
 
-const Slider = () =>{
-    return(
-        <>
-        <div className={style.wrapper}>
-            <Card/>
-            </div>
-        </>
-    )
-}
+
+// const Slider = () =>{
+//     const[results, getResult]=useState({})
+    
+//     cosnt ani = getAnime()
+//     return(
+//         <>
+//         <div className={style.wrapper}>
+//             <Card />
+//             </div>
+//         </>
+//     )
+// }
 
 export default Slider
