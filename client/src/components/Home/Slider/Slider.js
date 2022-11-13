a586076ddee2c8477f67190236db16845f9bf8dc
jejerome <goyalajerome@gmail.com>
2022-11-13 17:02:23 +0800

    added top 100 anime

--- client/src/components/Home/Slider/Slider.js
+++ client/src/components/Home/Slider/Slider.js
@@ -2,47 +2,9 @@ import React, {useState, useEffect} from "react";
 import style from './Slider.module.css'
 import Card from "../Card/Card";
 import getAnime from "../../../api/fetch";
+import Title from "../../Title/Title";
 
 
-// class Slider extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {results: []}
-        
-//     }
-    
-//     async componentDidMount(){
-//         const getAani = await this.getAnime('https://api.jikan.moe/v4/top/anime', {filter: 'bypopularity', page:1});
-//         this.setState({results: getAani},()=>{console.log(this.state.results[0])});
-//     }
-    
-
-//     async componentWillUnmount(){
-//         clearInterval(await this.getAani)
-//     }
-//      getAnime = async (url, params={}) => {
-//         const results = await axios({
-//             url: url,
-//             method: 'GET',
-//             params: params
-//         })
-        
-    
-//         return results.data.data
-//     }
-    
-//     render(){
-//         const {results} = this.state
-//         const sliced = results.slice(0,7)
-//         return(
-//         <div className={style.wrapper}>
-//             {sliced.map(ani=>(<Card anime={ani}  key={ani.mal_id}/>))}
-            
-//         </div>
-//         )
-//     }
-
-// }
 
 const Slider = () =>{
     const[results, setResult]=useState([])
@@ -51,17 +13,21 @@ const Slider = () =>{
         const sznAni = await getAnime('https://api.jikan.moe/v4/seasons/now', {page:1})
         setResult(sznAni.slice(0,7))
     }
+    
+
     useEffect(()=>{
         getSznAni()
     },[])
-
     
 
     
     return(
         <>
-        <div className={style.wrapper}>
-            {results.map(ani=>(<Card anime={ani} key={ani.mal_id}/>))}
+        <div className={style.title}>
+            <Title title={'Top season anime'}/>
+            <div className={style.wrapper}>
+                {results.map(ani=>(<Card anime={ani} key={ani.mal_id}/>))}
+            </div>
         </div>
         </>
     )
