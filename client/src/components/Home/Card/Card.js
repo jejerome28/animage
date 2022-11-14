6fd449d558052bf8c68a8d81a28461c36067fac7
jejerome <goyalajerome@gmail.com>
2022-11-14 20:58:32 +0800

    added details component and some style fix

--- client/src/components/Home/Card/Card.js
+++ client/src/components/Home/Card/Card.js
@@ -5,7 +5,9 @@ const Card = ({anime})=>{
     return(
         <>
         <div className={style.card}>
+            <a href="/#">
             <img src={anime.images.webp.image_url} alt='anime pic' ></img>
+            </a>
             <p>{anime.title}</p>
         </div>
         </>
