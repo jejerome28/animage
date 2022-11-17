5d871a5dbeee735a6b6e425721a61a931e1c609e
jejerome <goyalajerome@gmail.com>
2022-11-17 23:01:11 +0800

    get anime details from the backend

--- client/src/actions/getData.js
+++ client/src/actions/getData.js
@@ -1,4 +1,5 @@
 import getAnime from "../api/fetch";
+import { useParams } from "react-router-dom";
 
 const url = 'http://localhost:5000';
 
@@ -6,10 +7,17 @@ const url = 'http://localhost:5000';
 export const getTopAni = async (set_state)=> {
     const topAni = await getAnime(`${url}/`);
     set_state(topAni.topAnimes);
-    console.log(topAni);
 }
 
 export const getSznAni = async (set_state)=> {
-    const sznAni = await getAnime(`${url}/`)
-    set_state(sznAni.seasonAnime.slice(0,10))
+    const sznAni = await getAnime(`${url}/`);
+    set_state(sznAni.seasonAnime.slice(0,10));
+    console.log(sznAni);
+}
+
+// to get the anime details with the use of id 
+export const getDetails = async(set_state, id) => {
+    const details = await getAnime(`${url}/details/${id}`);
+    set_state(details);
+    console.log(details.ani_details);
 }
--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { getDetails } from "../../actions/getData";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Details = ()=>{
 
+    const [allDetails, setDetails]= useState([])
 
+    const {id} = useParams();
+    useEffect(()=>{
+        getDetails(setDetails,id);
+    },[id])
+
+    //TODO use the objects rendered to display details
+    //not working right now check how to render object using object keys tom
     return(
         <>
-        <h1>Details page</h1>
+        <h1>Details</h1>
+        <p>{allDetails.ani_details.title_english}</p>
         </>
     )
 }
