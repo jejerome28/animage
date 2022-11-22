270ef16f766a410c93f5e175780caba59032b521
jejerome <goyalajerome@gmail.com>
2022-11-22 16:52:54 +0800

    added styling

--- client/src/components/Details/components/Characters.js
+++ client/src/components/Details/components/Characters.js
@@ -7,19 +7,23 @@ const Characters = ({casts})=> {
     return(
         <>
         <div className={style.characters}>
-            <img src={character.images.webp.image_url} 
-            alt={character.name + ' pic'}></img>
-            <p>{character.name} - {casts.role}</p>
+            <div className={style.cast}>
+                <img src={character.images.webp.image_url} 
+                alt={character.name + ' pic'}></img>
+                <p>{character.name} - {casts.role}</p>
+            </div>
             
-            {voice_actors.map(va=>(
-                <img src={va.person.images.jpg.image_url}
-                alt={va.person.name + ' pic'} key={va.person.mal_id}>
-                </img> 
-            )).slice(0,1)}
-            
-            {voice_actors.map(va=>(
-                <p key={va.person.mal_id}>{va.person.name}</p>
-            )).slice(0,1)}
+            <div className={style.va}>
+                {voice_actors.map(va=>(
+                    <img src={va.person.images.jpg.image_url}
+                    alt={va.person.name + ' pic'} key={va.person.mal_id}>
+                    </img> 
+                )).slice(0,1)}
+                
+                {voice_actors.map(va=>(
+                    <p key={va.person.mal_id}>{va.person.name}</p>
+                )).slice(0,1)}
+            </div>
             
         </div>
         </>
