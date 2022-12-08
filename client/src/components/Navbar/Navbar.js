212e0bd77648a9f3ec5b51c42fd83cc87a83b8d8
jejerome <goyalajerome@gmail.com>
2022-12-08 13:36:28 +0800

    convert from sass to tailwindcss

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
-import style from './Navbar.module.scss'
+// import style from './Navbar.module.scss'
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return(
         <>
-        <div className={style.wrapper + ' bg-slate-400'}>
-            <Link to='/'>Logo</Link>
-            <Link to='/'>Home</Link>
-            <Link to='/browse'>Browse</Link>
-            <Link to='/forum'>Forum</Link>
-            <button className="login">Login</button>
+        <div className= 'flex justify-center bg-complement1 mb-4'>
+            <Link to='/' className="text-accent1 text-sm font-bold m-4 mr-auto ml-36">LOGO</Link>
+            <Link to='/' className="text-accent1 text-sm font-bold m-4">HOME</Link>
+            <Link to='/browse' className="text-accent1 text-sm font-bold m-4">BROWSE</Link>
+            <Link to='/forum' className="text-accent1 text-sm font-bold m-4">FORUM</Link>
+            <Link to='/login' className="text-accent1 text-sm font-bold m-4 ml-auto mr-36">LOGIN</Link>
         </div>
         </>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'main-color': '#ADD8E6',
+      'accent1': '#96AFB8',
+      'complement1': '#033D51',
+    }
   },
   plugins: [],
 }
