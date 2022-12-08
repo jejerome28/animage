a7971a67e656af90c20c61e0d99f1757470dba9f
jejerome <goyalajerome@gmail.com>
2022-12-08 23:59:42 +0800

    convert layout to tailwindcss

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React from "react";
-import style from './Layout.module.scss'
 import Home from "../Home/Home";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
@@ -9,20 +8,14 @@ import {  Routes, Route} from 'react-router-dom'
 const Layout = ()=> {
     return(
         <>
-        <div className={style.wrapper + ' font-sans bg-main-color'}>
-            <div className={style.nav}>
+        <div className= 'font-sans bg-main-color'>
                 <Navbar/>
-            </div>
-            <div className={style.home}>
                 <Routes>
                     <Route index element={<Home/>}/>
                     {/* to show details components when an anime link is clicked */}
                     <Route path="/details/:id" element={<Details/>}/>
                 </Routes>
-            </div>
-            <div className={style.footer}>
                 <Footer/>
-            </div>
         </div>
         </>
     )
