c7ebd9e14d10b0cb1d56f5ab56d87dbaa74b2728
jejerome <goyalajerome@gmail.com>
2022-12-11 12:40:18 +0800

    create login page

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -4,16 +4,18 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Details from "../Details/Details";
 import {  Routes, Route} from 'react-router-dom'
+import Login from "../Login/Login";
 
 const Layout = ()=> {
     return(
         <>
-        <div className= 'font-sans bg-main-color'>
+        <div className= 'font-sans bg-main-color min-h-screen'>
                 <Navbar/>
                 <Routes>
                     <Route index element={<Home/>}/>
                     {/* to show details components when an anime link is clicked */}
                     <Route path="/details/:id" element={<Details/>}/>
+                    <Route path="/login" element={<Login/>}/>
                 </Routes>
                 <Footer/>
         </div>
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Title from "../Title/Title";
+
+
+export default function Login () {
+    return(
+        <>
+            <div className="m-auto w-fit min-h-screen ">
+                <Title title={'Login page'}/>
+                <form method="POST"action="/login" className="p-4 bg-accent2 rounded-md drop-shadow-xl">
+                    <input type={'text'} id='name' placeholder="Username" className="block mb-4 w-full"></input>
+                    <input type={'password'} id='password' placeholder="Password" className="block mb-4 w-full"></input>
+                    <button type="submit" className="bg-accent1 p-1 rounded-md mr-4">Login</button>
+                    <a href="/">{"Don't have an account?"}</a>
+                </form>
+            </div>
+        </>
+    )
+}
