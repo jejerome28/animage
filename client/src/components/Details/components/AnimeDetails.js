1eb5d8cb8ea98c0a32356a2d7e7b425a527597e8
jejerome <goyalajerome@gmail.com>
2022-12-14 21:23:38 +0800

    minor fixes

--- client/src/components/Details/components/AnimeDetails.js
+++ client/src/components/Details/components/AnimeDetails.js
@@ -10,8 +10,8 @@ const AnimeDetails = ()=>{
     const url = `http://localhost:5000/details/${id}`;
     const {ani_details} = useFetch(url);
     
-    // const {genres} = ani_details;
-    // const {studios} = ani_details;
+    const {genres} = ani_details;
+    const {studios} = ani_details;
     return(
         <>
         {ani_details  ? (
@@ -21,10 +21,10 @@ const AnimeDetails = ()=>{
                     <p className="font-semibold">Date aired: </p><p className="text-accent1 mb-4">{ani_details.aired.string}</p>
                     <p className="font-semibold">Score: </p> <p className="text-accent1 mb-4">{ani_details.score}</p>
                     <p className="font-semibold">Rating: </p><p className="text-accent1 mb-4">{ani_details.rating}</p>
-                    {/* <p className="font-semibold">Studios: </p>{studios.map(studio => (<p className="text-accent1 mb-4" key={studio.mal_id}>{studio.name} </p>))} */}
+                    <p className="font-semibold">Studios: </p>{studios.map(studio => (<p className="text-accent1 mb-4" key={studio.mal_id}>{studio.name} </p>))}
                     <p className="font-semibold">Episodes: </p><p className="text-accent1 mb-4">{ani_details.episodes}</p>
                     <p className="font-semibold">Status: </p><p className="text-accent1 mb-4">{ani_details.status}</p>
-                    {/* <p className=" font-semibold mb-2">Genres: </p>{genres.map(genre => (<p className="text-accent1" key={genre.mal_id}>{genre.name} </p>))} */}
+                    <p className=" font-semibold mb-2">Genres: </p>{genres.map(genre => (<p className="text-accent1" key={genre.mal_id}>{genre.name} </p>))}
                 </div>
 
             </div>
