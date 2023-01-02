834e6a1ca9506e702218d9d4b3cb37c78139b983
jejerome <goyalajerome@gmail.com>
2023-01-02 16:56:18 +0800

    add rate limit

--- server/index.js
+++ server/index.js
@@ -11,8 +11,15 @@ const storeSession = mongoStore.create({mongoUrl:dbString, collectionName: 'sess
 const routes = require('./routes');
 const method_override = require('method-override');
 const {auth} = require('./controller');
-const cors = require('cors')
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 60, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -34,11 +41,14 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(limiter)
 
 auth(
     passport,
     username => user.findOne({username})
 )
+
+
 
 //create custom connection
 mongoose.createConnection(dbString, (err,result)=>{if(err)throw err; console.log('connected')})
