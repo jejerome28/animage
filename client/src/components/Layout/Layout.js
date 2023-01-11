62b0c97c1e6ba987ae0e8255342381d612ebfa31
jejerome <goyalajerome@gmail.com>
2023-01-11 13:04:28 +0800

    created context for login functionality

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -7,23 +7,67 @@ import {  Routes, Route} from 'react-router-dom'
 import Login from "../Login/Login";
 import Signup from "../Login/Signup";
 import Browse from "../Browse/Browse";
+import { useState } from "react";
+import axios from 'axios';
+import { LoggedUser } from "./loggedContext";
 
 const Layout = ()=> {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState('');
+
+    //store in an object to be sent to the server
+    const results = {username: username, password:password} ;
+
+    //send the object to the server for storage
+    const handleSubmit = async (e) =>{
+        try{
+        e.preventDefault();
+
+            const res = await axios({
+                method:'POST',
+                data: results,
+                withCredentials:true,
+                url: '/login'
+            })
+            
+            if(res.status === 200){
+                setUser(res.data.username);
+                console.log('form sent');
+            }
+            else{
+                console.log('error occured');
+            }
+            console.log(res)
+        }
+        catch(e){
+            console.log(e.message)
+        }
+        // postNewUser('/login', results);
+        // console.log(results);
+        
+    }
     return(
         <>
         <div className= 'font-sans bg-main-color min-h-screen'>
+        <LoggedUser.Provider value={user}>
                 <Navbar/>
                 <div className="flex flex-col items-center">
                     <Routes>
                         <Route index element={<Home/>}/>
                         {/* to show details components when an anime link is clicked */}
                         <Route path="/details/:id" element={<Details/>}/>
-                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/login" 
+                            element={<Login setUsername={setUsername} 
+                            setPassword={setPassword}
+                            handleSubmit={handleSubmit}
+                        />}/>
                         <Route path="/signup" element={<Signup/>}/>
                         <Route path="/browse" element={<Browse/>}/>
                     </Routes>
                     <Footer/>
                 </div>
+        </LoggedUser.Provider>
         </div>
         </>
     )
