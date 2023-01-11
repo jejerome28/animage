7252bbcee3fd26ce14b4ead01cdd468c20b5f632
jejerome <goyalajerome@gmail.com>
2023-01-11 13:05:07 +0800

    render current logged in user

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
 // import style from './Navbar.module.scss'
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { LoggedUser } from "../Layout/loggedContext";
 
 const Navbar = () => {
+    const user = useContext(LoggedUser);
+    console.log(user);
     return(
         <>
         <div className= 'flex justify-center bg-complement1 mb-14 drop-shadow-xl tracking-widest'>
@@ -10,7 +14,11 @@ const Navbar = () => {
             <Link to='/' className="text-accent1 text-sm font-bold m-4">HOME</Link>
             <Link to='/browse' className="text-accent1 text-sm font-bold m-4">BROWSE</Link>
             <Link to='/forum' className="text-accent1 text-sm font-bold m-4">FORUM</Link>
-            <Link to='/login' className="text-accent1 text-sm font-bold m-4 ml-auto mr-36">LOGIN</Link>
+            {
+                user ? <Link to='/login' className="text-accent1 text-sm font-bold m-4 ml-auto mr-36">{user}</Link> 
+                :
+                <Link to='/login' className="text-accent1 text-sm font-bold m-4 ml-auto mr-36">LOGIN</Link>
+            }
         </div>
         </>
     )
