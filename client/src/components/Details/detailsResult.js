f67b2086088e07eb86db7b115ba18f6825fd74da
jejerome <goyalajerome@gmail.com>
2023-01-11 13:36:53 +0800

    some fixes

--- client/src/components/Details/detailsResult.js
+++ client/src/components/Details/detailsResult.js
@@ -1,12 +1,4 @@
 import { createContext } from "react";
-// import useFetch from "../../actions/useFetch";
-// import { useParams } from "react-router-dom";
-
-// const {id} = useParams();
-// const url = `http://localhost:5000/details/${id}`;
-
-
-// const {ani_details} = useFetch(url);
 
 export const DetailsResult = createContext()
 
