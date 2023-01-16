73eb6a61dbf937a72fc37f5892103f528c329613
jejerome <goyalajerome@gmail.com>
2023-01-16 13:37:57 +0800

    created link to commenter profile

--- client/src/components/Details/components/Comments.js
+++ client/src/components/Details/components/Comments.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from 'prop-types'
 import Title from "../../Title/Title";
+import { Link } from "react-router-dom";
 
 const Comments = ({comments})=>{
     
     return(
         <>
             <Title title={'Comments'}/>
-            {comments ?  comments.map(comment => ( <p key={comment._id}>{comment.body} ----- <span>{comment.user_id.username}</span></p>)) : (<h1>loading</h1>) }
+            {comments ?  comments.map(comment => ( <p key={comment._id}>{comment.body} ----- <Link to={`/profile/${comment.user_id._id}`}>{comment.user_id.username}</Link></p>)) : (<h1>loading</h1>) }
         </>
     )
 }
