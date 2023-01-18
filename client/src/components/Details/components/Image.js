bfad6042bc3b0ac7b1f2bdba33eb67d9d1f8efdf
jejerome <goyalajerome@gmail.com>
2023-01-18 14:58:25 +0800

    added button for saving playlist

--- client/src/components/Details/components/Image.js
+++ client/src/components/Details/components/Image.js
@@ -2,21 +2,30 @@ import React from "react";
 import Title from "../../Title/Title";
 import PropTypes from 'prop-types';
 import { useContext } from "react";
-import { DetailsResult } from "../detailsResult";
+import { LoggedUser } from "../../Layout/loggedContext";
+import axios from 'axios';
 
-const Image = ()=>{
-    //use anime details result context
-    const {ani_details} = useContext(DetailsResult);
+const Image = ({ani_details, id})=>{
+    //use anime details result 
+    const user = useContext(LoggedUser);
+    
+    const handleClick = async () => {
+        try{
+            const url = `/update_profile/${user._id}`;
+            
+            //data to be sent to backend
+            const data = {anime_id:id, ani_title: ani_details.title_english, userId:user._id};
+            await axios.post(url,data);
+        }
+        catch(e){
+            console.log(e.message)
+        }
+    }
     return(
         <>
-        
-        {ani_details && 
-            <>
             <Title title={ani_details.title_english}/>
-            <img src={ani_details.images.webp.image_url} className='h-97 rounded-lg w-60 mb-12' alt={'anime pic'}></img>
-            </>
-        || (<h1>loding</h1>)
-        }
+            <img src={ani_details.images.webp.image_url} className='h-97 rounded-lg w-60 mb-3 ' alt={'anime pic'}></img>
+            {user && <button className="mb-12 mr-4" onClick={handleClick}>Add</button>}
             
         </>
 
@@ -25,7 +34,8 @@ const Image = ()=>{
 }
 
 Image.propTypes = {
-    ani_details: PropTypes.object
+    ani_details: PropTypes.object,
+    id: PropTypes.string
 }
 
 export default Image;
