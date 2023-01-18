4ce4a064459d0cc6bad23234d0afeb64f8f79ce7
jejerome <goyalajerome@gmail.com>
2023-01-18 14:59:21 +0800

    fix updated comment problems

--- client/src/components/Details/components/InputArea.js
+++ client/src/components/Details/components/InputArea.js
@@ -1,30 +1,39 @@
 import React from "react"
 import PropTypes from 'prop-types'
-import { useState, useContext } from "react"
+import { useState, useEffect } from "react"
 import axios from 'axios'
-import { DetailsResult } from "../detailsResult"
 import Comments from "./Comments"
 
-const InputArea = ({children})=> {
-    //get context and set as initial value of the state of comments
-    const {id, comments} = useContext(DetailsResult);
-    const [comment, setComment] = useState(comments);
+const InputArea = ({id, comments, children})=> {
+    
+    // set initial value of the state of comments
+    const [comment, setComment] = useState([]);
     const [input, setInput ] = useState('');
+    
+    //get the comments from db
+    useEffect(()=>{
+        try{
+            setComment(comments);
+        }
+        catch(e){
+            console.log(e)
+        }
+    },[])
 
     //to send the comment and receive the updated comments then set as the comment state
     const handleSubmit = async(e)=>{
         try{
             e.preventDefault();
             
-            const {data} = await axios({
+            const res = await axios({
                 method: 'POST',
                 data: {addComment:input},
                 url: `/details/${id}/post_comment`
             })
            
-            setComment(data);
+            setComment(res.data.comments);
             setInput('');
-            console.log(data);
+            console.log(res);
         }
         catch(e){
             console.log(e.message)
@@ -34,8 +43,9 @@ const InputArea = ({children})=> {
     return(
         <>
             {children}
-            {/* if the comment is undefined, use the context first as initial state */}
-            {comment && <Comments comments={comment}/> || <Comments comments={comments}/>}
+        
+            <Comments comments={comment}/>
+        
 
             <form onSubmit={handleSubmit}>
                 <textarea className='resize-none w-full rounded-md p-1' onChange={e=>setInput(e.target.value)} value={input} placeholder="Tell others what do you think...">
@@ -47,7 +57,9 @@ const InputArea = ({children})=> {
 }
 
 InputArea.propTypes = {
-    children: PropTypes.any
+    children: PropTypes.any,
+    comments: PropTypes.array,
+    id: PropTypes.string
 }
 
 export default InputArea
