30c295bcfc8691b0c36db733787908c75b32d2e3
jejerome <goyalajerome@gmail.com>
2023-01-19 03:27:46 +0800

    fixed card flex basis

--- client/src/components/Home/Card.js
+++ client/src/components/Home/Card.js
@@ -7,10 +7,10 @@ const Card = ({ani_details,children})=>{
     return(
         <>
         {/* render acquired results to elements */}
-            <div className='flex w-slider-w mb-12'>
+            <div className='flex w-slider-w mb-12 flex-wrap'>
             {/* {ani_details ? ani_details.map(anime =>  */}
             {ani_details.map(anime => 
-            <div key={anime.mal_id} className='mr-4 items-center w-36 drop-shadow-xl cursor-pointer hover:scale-125 ease-in duration-300'>
+            <div key={anime.mal_id} className='mr-4 items-center w-36 drop-shadow-xl cursor-pointer hover:scale-125 ease-in duration-300 basis-32'>
                 <Link  to={`/details/`+ anime.mal_id}>
                     <img src={anime.images.webp.image_url} className='rounded-md w-36 h-56 mb-4' alt='anime pic' ></img>
                     <p className="text-md text-complement1">{anime.title}</p>
