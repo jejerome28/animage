25c5a369dfc0dd190edb8e17bbbce607fc0033d6
jejerome <goyalajerome@gmail.com>
2023-01-18 15:00:25 +0800

    created users page

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -7,23 +7,23 @@ import Title from '../Title/Title';
 const User = ()=>{
     const {id} = useParams();
     const url = `/profile/${id}`;
-    const user = useFetch(url);
-
+    const {username, comments, playlist} = useFetch(url);
+    
     return(
         <>
-            <Title title={user.username + "'s profile"}/>
+            <Title title={username + "'s profile"}/>
 
             <Title title={'Bio'}/>
             <div className='bg-accent2 p-4 text-accent1 font-regular drop-shadow-xl leading-6 rounded-md mb-12'>lorem ipsun</div>
 
             <Title title={'Anime Playlist'}/>
             <div className='bg-accent2 p-4 text-accent1 font-regular drop-shadow-xl leading-6 rounded-md mb-12'>
-               {/* {user && user.comments.map(comment => {<ListItem ani_details={comment}/>})}  */}
+                {playlist && playlist.map(playlist => (<p key={playlist._id}>{playlist.body}</p>))}
             </div>
 
             <Title title={'Comments made'}/>
             <div className='bg-accent2 p-4 text-accent1 font-regular drop-shadow-xl leading-6 rounded-md mb-12'>
-                {user && user.comments.map(comment => (<p key={comment._id}>{`" ${comment.body} "`} on anime: </p>))}
+                {comments && comments.map(comment => (<p key={comment._id}>{`" ${comment.body} "`} on anime: </p>)) }
             </div>
         </>
 
