9729dc7b16aaff6b0eae454e9697294d4bf0caab
jejerome <goyalajerome@gmail.com>
2023-01-21 17:58:50 +0800

    implemented skeleton loading for home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Card from "./Card";
-import ListItem from "./ListItem";
+import ListItem from"./ListItem";
 import useFetch from "../../actions/useFetch";
 import Title from "../Title/Title";
+import LoadingCard from "../Core/LoadingCard";
+import LoadingList from "../Core/LoadingList";
 
 const Home = () =>{
     const url = 'http://localhost:5000';
@@ -11,10 +13,11 @@ const Home = () =>{
         <>
         <div className="flex flex-col w-slider-w mb-12 mt-12">
             <Title title={'Top Season Anime'}/>
-            {seasonAnime && <Card ani_details={seasonAnime.slice(0,6)}/>}
+            {seasonAnime && <Card ani_details={seasonAnime.slice(0, 6)} /> || <LoadingCard number={6}/>}
             
             <Title title={'Top Popular Anime'}/>
-            {topAnimes && <ListItem ani_details={topAnimes}/>}
+                {topAnimes && <ListItem ani_details={topAnimes.slice(0,10)}/> || <LoadingList number={10}/>}
+
             
         </div>
         </>
