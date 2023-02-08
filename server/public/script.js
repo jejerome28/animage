74f3a9297c2947ef5612838db7169ad8d320a3f4
jejerome <goyalajerome@gmail.com>
2023-02-08 10:36:12 +0800

    remove static files

--- server/public/script.js
+++
@@ -1,8 +0,0 @@
-const counter  = () => {
-    let count = document.getElementById('addComment');
-    document.getElementById('counter').innerText = 'hello';
-}
-
-document.getElementById("counter").innerHTML = "hello";
-document.getElementsByClassName('add_comment').getElementsById('counter').innerHTML = 'hello';
-alert('hello');
