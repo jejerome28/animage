4a5912976e361aba6366c55bd77dec2600d4ff8f
jejerome <goyalajerome@gmail.com>
2023-02-08 09:12:00 +0800

    serve static html

--- server/index.js
+++ server/index.js
@@ -22,9 +22,10 @@ const limiter = rateLimit({
 })
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static('client/build'));
+    app.use(express.static('/public'))
+    // app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    // app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
 
 app.set('views', path.join(__dirname, 'views'));
