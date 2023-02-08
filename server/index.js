8cf7a6d46555c864ba273a2206bdc7bdf404e3d0
jejerome <goyalajerome@gmail.com>
2023-02-08 09:12:55 +0800

    fix file patth

--- server/index.js
+++ server/index.js
@@ -23,10 +23,11 @@ const limiter = rateLimit({
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
     app.use(express.static('/public'))
-    // app.use(express.static('client/build'));
 
-    // app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'public','index.html')));
   }
+
+console.log(__dirname);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
