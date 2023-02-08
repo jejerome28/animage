e93d1665afa18782b5aa76db061149c6df246cdb
jejerome <goyalajerome@gmail.com>
2023-02-08 13:03:06 +0800

    fix cors

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const limiter = rateLimit({
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
-app.use(cors({credentials:true}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
