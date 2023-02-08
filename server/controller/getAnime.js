b88cc216c0cd687bfb1d859e79d6a1b89e840eba
jejerome <goyalajerome@gmail.com>
2023-02-08 14:53:10 +0800

    fix cors

--- server/controller/getAnime.js
+++ server/controller/getAnime.js
@@ -149,7 +149,7 @@ const login =  (req, res)=>{
             else{
                 req.logIn(user,(err)=>{
                     if(err)throw err;
-                    res.header("Access-Control-Allow-Origin", "*")
+                    res.header("Access-Control-Allow-Origin", "https://animage.onrender.com")
                     res.status(200).json(req.user)
                     console.log(req.user);
                 })
