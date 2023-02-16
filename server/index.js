ebf079cb5959460d44d60963bcd847b03fed3e94
jejerome <goyalajerome@gmail.com>
2023-02-16 09:14:37 +0800

    allowed origin

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const limiter = rateLimit({
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
-app.use(cors());
+app.use(cors({origin:"*"}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
