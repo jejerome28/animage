7346f9aa07e67d034447c482f3457c84ca025954
jejerome <goyalajerome@gmail.com>
2023-02-21 23:05:25 +0800

    set hovering to md devices only

--- client/src/components/Home/ListItem.js
+++ client/src/components/Home/ListItem.js
@@ -9,7 +9,7 @@ const ListItem = ({ani_details,children})=>{
 
         {/*map thru the array of objects and render in to the elements*/}
         {ani_details.map(anime => 
-        <Link className="flex mb-4 mx-2 bg-accent2 rounded-md cursor-pointer hover:scale-110 ease-in duration-300" key={anime.mal_id} to={`/details/`+ anime.mal_id}>
+        <Link className="flex mb-4 mx-2 bg-accent2 rounded-md md:cursor-pointer md:hover:scale-110 md:ease-in md:duration-300" key={anime.mal_id} to={`/details/`+ anime.mal_id}>
                 <img src={anime.images.webp.image_url} alt='anime pic' className="mr-4 w-20 p-2 rounded-md" ></img>
                 <div>
                     <section className="text-complement1 mb-12 mt-1 font-semibold">
