8699fecd9f095a9f5d78dbfa92fac2aee0d5df54
jejerome <goyalajerome@gmail.com>
2023-02-21 22:40:11 +0800

    added margin

--- client/src/components/Title/Title.js
+++ client/src/components/Title/Title.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Title = ({title})=>{
     return(
         <>
-        <h2 className='text-2xl mb-4 text-complement1 font-semibold'>
+        <h2 className='text-2xl mb-4 mx-2 text-complement1 font-semibold'>
             {title}
         </h2>
         </>
