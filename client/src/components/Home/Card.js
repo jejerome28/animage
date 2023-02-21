c370aa2781d45403a7f7e0a7461e88527b3e382c
jejerome <goyalajerome@gmail.com>
2023-02-21 22:48:34 +0800

    some fixes

--- client/src/components/Home/Card.js
+++ client/src/components/Home/Card.js
@@ -11,7 +11,7 @@ const Card = ({ani_details,children})=>{
 
                 {ani_details.map(anime => 
                     <Link key={anime.mal_id} className=" block p-4 basis-4/12 drop-shadow-xl cursor-pointer hover:scale-125 ease-in duration-300 md:basis-32 md:p-0 md:mr-4" to={`/details/`+ anime.mal_id}>
-                            <img src={anime.images.webp.image_url} className='rounded-md max-w-xs max-h-48 object-contain md:max-h-56 md:max-w-md mb-4' alt='anime pic' ></img>
+                            <img src={anime.images.webp.image_url} className='rounded-md max-w-xs max-h-56 object-contain md:max-h-56 md:max-w-md mb-4' alt='anime pic' ></img>
                             <p className="text-sm md:text-base text-complement1">{anime.title}</p>
                     </Link>
                 )}
